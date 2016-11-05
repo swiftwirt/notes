b052b286372274a8a28b670b628960995a662099
Dmitry Ivashin <dmitroivashyn@gmail.com>
2016-11-05 22:39:45 +0200

    Initial Commit

---
+++ iOS_trainee_Cleveroad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS_trainee_Cleveroad
+//
+//  Created by Ivashin Dmitry on 11/5/16.
+//  Copyright © 2016 Ivashin Dmitry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
